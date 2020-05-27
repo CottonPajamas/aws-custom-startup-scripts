ccf7310bac530fed397d9b21dada1a35999327a7
CottonPajamas <30998454+CottonPajamas@users.noreply.github.com>
2020-05-27 10:40:52 +0800

    Update start.sh

--- squid/start.sh
+++ squid/start.sh
@@ -2,8 +2,17 @@
 yum update -y
 yum -y install squid
 systemctl stop squid
-# copy config from github
 
+# Create backup of the original squid.conf
+mv /etc/squid/squid.conf /etc/squid/squid.conf.bak
+
+# Downloading custom squid.conf
+curl https://cottonpajamas.github.io/aws-custom-startup-scripts/squid/squid.conf --output /etc/squid/squid.conf
+
+# Setting file permissions
+chmod 640 /etc/squid/squid.conf
+
+# Starting squid back up
 systemctl start squid
 systemctl enable squid
 systemctl status squid
